c92b524362399ccdc93fa6eb39e9867a29488187
pruthvi0918 <pruthviraj0918@gmail.com>
2021-09-20 00:21:22 -0700

    Reading CSV files from Zip folder and creating single combined file.

--- pruthvipycsv/smart_csv.py
+++ pruthvipycsv/smart_csv.py
@@ -6,22 +6,31 @@ from pathlib import Path
 from zipfile import ZipFile
 import configparser
 import pdb
+import logging
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[
+        logging.FileHandler("debug.log"),
+        logging.StreamHandler()
+    ]
+)
 
 
 class Smartcsv:
 
     def process_csv_files(self):
         """ process csv files to create single combined file"""
-        print("Statrted reading csv files")
+        logging.info("Statrted reading csv files")
 
         data_file_path_obj = self.get_data_file_path()
 
         if(not data_file_path_obj.exists()):
-            print(
+            logging.info(
                 "Data file is missing to process please place .zip file under data folder")
         else:
-            print(f"Started processing the {data_file_path_obj}  file")
-            print(
+            logging.info(f"Started processing the {data_file_path_obj}  file")
+            logging.info(
                 f" extracted data under below folder:  {data_file_path_obj.stem}")
             old_dir_path = Path()/data_file_path_obj.stem
             if old_dir_path.exists() and old_dir_path.is_dir():
@@ -36,19 +45,20 @@ class Smartcsv:
 
         thisfolder = os.path.dirname(os.path.abspath(__file__))
         initfile = os.path.join(thisfolder, r'configuration.txt')
-        # print(f"Current folder : {thisfolder}")
-        # print(initfile)
+        # logging.info(f"Current folder : {thisfolder}")
+        # logging.info(initfile)
         config = configparser.ConfigParser()
         config.read(initfile)
         test_file_data_path = config.get(
             'FilePath-info', 'test_file_data_path')
-        print(f" Reading test data from this location : {test_file_data_path}")
+        logging.info(
+            f" Reading test data from this location : {test_file_data_path}")
 
         data_file_path_obj = Path(os.path.join(
             thisfolder, "..", test_file_data_path))
         # /Users/pruthviraj/Python/Pruthvi_Py_CSV/pruthvipycsv
         # /Users/pruthviraj/Python/Pruthvi_Py_CSV/data/Engineering Test Files.zip
-        print(data_file_path_obj)
+        logging.info(data_file_path_obj)
         return data_file_path_obj
 
     def write_csv_files_to_single_file(self, path_extracted_data, csv_files):
@@ -60,7 +70,7 @@ class Smartcsv:
                 csvwriter.writerow(fields)
                 for eachcsvfile in csv_files:
                     if not eachcsvfile.name == "Combined.csv":
-                        print(
+                        logging.info(
                             f" current file processing is .... {path_extracted_data/eachcsvfile.name}")
                         with open(os.path.join(path_extracted_data/eachcsvfile.name), "r", encoding="utf-8-sig") as csvfile:
                             reader = csv.DictReader(csvfile)
@@ -69,11 +79,11 @@ class Smartcsv:
                                     row["Source IP"], eachcsvfile.name]
                                 csvwriter.writerow(
                                     data_to_write)
-                        print(
+                        logging.info(
                             f"*** Processing completed for file: {path_extracted_data/eachcsvfile.name} ***")
             return True
         except Exception as ex:
-            print("Something went wrong when writing to the file")
+            logging.error("Something went wrong when writing to the file")
             return False
 
     def get_csv_files(self, path_obj):
@@ -81,7 +91,7 @@ class Smartcsv:
         with ZipFile(path_obj) as zip_obj:
             zip_obj.extractall(path_obj.stem)
             path_of_extracted_data = Path()/path_obj.stem/path_obj.stem
-            print(path_of_extracted_data.is_dir())
+            logging.info(path_of_extracted_data.is_dir())
             csv_files = [
                 each_item for each_item in path_of_extracted_data.rglob("*.csv")]
         return path_of_extracted_data, csv_files
